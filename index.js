c068b4ef0bd7dd53521cb58726b54464a9350a69
pyroshary <sheharyarghulamrasool@gmail.com>
2024-03-21 01:58:43 +0500

    commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//making a funtion to calculte rectangle area
+function majorarea(width, height) { return width * height; }
+;
+//making arrow function to refactored data
+var area = function (width, height) { return width * height; };
+console.log(area(5, 10));
---
+++ index.ts
@@ -0,0 +1,6 @@
+//making a funtion to calculte rectangle area
+function majorarea( width: number, height:number): number{return width*height};
+//making arrow function to refactored data
+let area =( width: number, height:number): number => width*height;
+
+console.log(area(5,10));
